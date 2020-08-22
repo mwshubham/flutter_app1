b97261044e4775d897ab341481d9b8d74315515f
Shubham Agarwal <mwshubham@gmail.com>
2020-08-22 15:54:45 +0530

    Added english_words dependency and usage.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,7 +6,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -39,6 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -52,6 +53,7 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            Text(wordPair.asPascalCase),
           ],
         ),
       ),
